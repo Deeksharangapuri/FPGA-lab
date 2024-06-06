47106177e7796b6a310cbc25ea12c3cbf0d1e32a
Deeksha Rangapuri <drangapu@qti.qualcomm.com>
2024-06-06 11:07:03 +0530

    added demo.py and changed assign1.py

--- assgn1.py
+++ assgn1.py
@@ -2,15 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN)
-GPIO.setup(15, GPIO.IN)
-GPIO.setup(16, GPIO.IN)
-GPIO.setup(18, GPIO.IN)
-print(GPIO.input(12))
-print(GPIO.input(15))
-print(GPIO.input(16))
-print(GPIO.input(18))
+
 
 no = str(GPIO.input(12))+str(GPIO.input(15))+str(GPIO.input(16))+str(GPIO.input(18));
 
